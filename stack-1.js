3ffe6745cd6747e6763974d12084b13962d58165
Thamarai-IND <thamarai.vivid@gmail.com>
2026-03-16 22:29:22 +0530

    code beautification and peek and empty stack condition included

--- stack-1.js
+++ stack-1.js
@@ -7,15 +7,39 @@ function push(value) {
 }
 
 function pop() {
+
+    if( top == -1) 
+        return 'Stack is empty';
     removed = stack[top];
     top = top - 1;
     return removed
 }
 
-push(10);
-push(20);
-push(30);
-push(40);
-push(50);
-console.log("stack : ",stack);
+function printStack() {
+
+    if( top == -1)
+        return 'Stack is empty';
+    let result = '';
+
+    for(let i=0; i<=top; i++) {
+        result += stack[i];
+        if(i<top) result += ' > ';
+    }
+    return result;
+}
+
+function peek() {
+    if( top == -1)
+        return 'Stack is empty';
+    return stack[top];
+}
+
+// push(10);
+// push(20);
+// push(30);
+// push(40);
+// push(50);
+console.log("initial stack : ",stack);
 console.log("pop : ",pop());
+console.log("print stack: ",printStack());
+console.log("peek : ",peek());
